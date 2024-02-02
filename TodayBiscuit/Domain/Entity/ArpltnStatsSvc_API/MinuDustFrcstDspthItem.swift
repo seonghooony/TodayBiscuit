dd1983bf7bdcdbfef02e85c24649c5b857d46e29
outside_SH <hanrinsa2@naver.com>
2024-02-02 18:01:53 +0900

    공공데이터 엔티티 설계

--- TodayBiscuit/Domain/Entity/ArpltnStatsSvc_API/MinuDustFrcstDspthItem.swift
+++ TodayBiscuit/Domain/Entity/ArpltnStatsSvc_API/MinuDustFrcstDspthItem.swift
@@ -13,97 +13,97 @@ struct MinuDustFrcstDspthData: Codable {
      
      결과값 예시 : 2020-11-13 11시 발표
      */
-    let dataTime: String
+    let dataTime: String?
     /**
      예측통보시간
      
      결과값 예시 : 2020-11-14
      */
-    let informData: String
+    let informData: String?
     /**
      통보코드
      
      결과값 예시 : PM10
      */
-    let informCode: String
+    let informCode: String?
     /**
      예보개황
      
      결과값 예시 : ○[미세먼지] 수도권ㆍ강원영서...
      */
-    let infornOverall: String
+    let infornOverall: String?
     /**
      발생원인
      
      결과값 예시 : ○[미세먼지] 황사의 영향으로...
      */
-    let informCause: String
+    let informCause: String?
     /**
      예보등급
      
      결과값 예시 : 서울: 나쁨, 제주: 나쁨, 전남: 나쁨...
      */
-    let informGrade: String
+    let informGrade: String?
     /**
      행동요령
      
      결과값 예시 :
      */
-    let actionKnack: String
+    let actionKnack: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl1: String
+    let imageUrl1: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl2: String
+    let imageUrl2: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl3: String
+    let imageUrl3: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl4: String
+    let imageUrl4: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl5: String
+    let imageUrl5: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl6: String
+    let imageUrl6: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl7: String
+    let imageUrl7: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl8: String
+    let imageUrl8: String?
     /**
      시간대별 예측모델 결과사진(6:00, 12:00, 18:00, 24:00 KST)
      
      결과값 예시 : https://www.airkorea.or.kr/dustImage/..
      */
-    let imageUrl9: String
+    let imageUrl9: String?
 }
 
 //공공데이터포털
